dd3fb272d658475e115dbeb7286f86a4b6787cc0
Hector <hjaramboles@bpstec.do>
2023-09-28 13:49:45 -0400

    nombresyapellidos by default

--- tests/000-nombresyapellidos-personas.ts
+++ tests/000-nombresyapellidos-personas.ts
@@ -1,22 +1,22 @@
 // Archivo para colocar los nombres de las personas a crear, necesarias para poder correr todos los tests
 
 // Persona Fisica Mayor de edad. Test = 006-registrar-persona-fisica.test.ts
-export const nombrePersonaFisica:string = 'TEA'; // (Femenino)
-export const apellidoPersonaFisica:string = 'PAVON';
+export const nombrePersonaFisica:string = ''; // (Femenino)
+export const apellidoPersonaFisica:string = '';
 
 // Persona Fisica Menor de edad. Test = 007-registrar-persona-fisica-menor-edad.test.ts
-export const nombrePersonaMenorEdad:string = 'JACINTO'; // (Masculino)
-export const apellidoPersonaMenorEdad:string = 'PAVON';
+export const nombrePersonaMenorEdad:string = ''; // (Masculino)
+export const apellidoPersonaMenorEdad:string = '';
 
 // Persona Juridica y Relacionado. Test = 008-registrar-persona-juridica.test.ts
-export const nombreJuridica:string = 'AGROPECUARIA DPD'; 
+export const nombreJuridica:string = ''; 
 
-export const nombreRelacionadoJuridica:string = 'DORIAN'; // (Masculino)
-export const apellidoRelacionadoJuridica:string = 'PERAZA';
+export const nombreRelacionadoJuridica:string = ''; // (Masculino)
+export const apellidoRelacionadoJuridica:string = '';
 
 // Persona Fisica Casada. Test = 010-registrar-persona-fisica-casada.test.ts
-export const nombrePersonaFisicaCasada:string = 'SANDY'; // (Femenino)
-export const apellidoPersonaFisicaCasada:string = 'ESQUEDA';
+export const nombrePersonaFisicaCasada:string = ''; // (Femenino)
+export const apellidoPersonaFisicaCasada:string = '';
 
-export const nombrePersonaFisicaConyuge:string = 'RAVAN'; // (Masculino)
-export const apellidoPersonaFisicaConyuge:string = 'PAYAN';
+export const nombrePersonaFisicaConyuge:string = ''; // (Masculino)
+export const apellidoPersonaFisicaConyuge:string = '';
